4e7874d7705577cfce97f668c1a1baa5de9178fa
Maria Jaqueline <mc0176765@gmail.com>
2023-09-04 22:11:49 -0300

    adicionado componente camera

--- Preservacao/app/src/main/java/com/example/preservacao/Cadastro.java
+++ Preservacao/app/src/main/java/com/example/preservacao/Cadastro.java
@@ -1,13 +1,20 @@
 package com.example.preservacao;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.core.app.NotificationCompat;
 
+import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.Random;
@@ -17,6 +24,8 @@ public class Cadastro extends AppCompatActivity {
     Button cad;
     TextView tx_map;
 
+    Bitmap bitmap;
+    ImageView imageView;
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +35,12 @@ public class Cadastro extends AppCompatActivity {
         cad = findViewById(R.id.btn_pess);
         tx_map = findViewById(R.id.text_map);
 
+        imageView = findViewById(R.id.edt_camera);
+        if (ActivityCompat.checkSelfPermission(Cadastro.this, android.Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
+
+            ActivityCompat.requestPermissions(Cadastro.this, new String[] {Manifest.permission.CAMERA},0);
+
+        }
         tx_map.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -47,6 +62,31 @@ public class Cadastro extends AppCompatActivity {
                 alerta.getManager().notify(new Random().nextInt(), builder.build());
             }
         });
+        imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Ligar();
+            }
+        });
 
     }
+    public void Ligar(){
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(intent, 1);
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 1 && resultCode == Cadastro.RESULT_OK ){
+
+            Bundle extras = data.getExtras();
+            bitmap = (Bitmap) extras.get("data");
+            imageView.setImageBitmap(bitmap);
+
+        }
+
+        super.onActivityResult(requestCode, resultCode,data);
+}
 }
